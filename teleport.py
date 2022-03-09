ba09ef1d0f9691661c80dd2453a649a810b1bd78
ArdaGurcan <ardagurcan2004@gmail.com>
2022-03-09 16:24:03 +0300

    add results and remove ibmq background

--- teleport.py
+++ teleport.py
@@ -2,11 +2,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, Aer, IBMQ, transpile
 from qiskit.providers.ibmq import least_busy
-from qiskit.visualization import plot_histogram
-from qiskit.quantum_info import Statevector, random_statevector, partial_trace
-
-from qiskit.extensions import Initialize
-from qiskit.visualization import plot_histogram, plot_bloch_vector, plot_bloch_multivector
+from qiskit.quantum_info import random_statevector, partial_trace
+from qiskit.visualization import plot_bloch_multivector
 
 
 qreg_anne = QuantumRegister(2, 'anne') # entangler
@@ -17,8 +14,10 @@ circuit = QuantumCircuit(qreg_anne, qreg_bob, qreg_clara, creg_bob)
 
 # initialize info_qubit with random value and send it to bob
 info_qubit = random_statevector(2)
+
 plot_bloch_multivector(info_qubit)
 plt.savefig("initial_qubit.svg")
+
 circuit.initialize(info_qubit, [qreg_bob[0]])
 circuit.barrier()
 
@@ -44,6 +43,10 @@ circuit.barrier()
 # apply gates if the depending on registers
 circuit.x(qreg_clara[0]).c_if(creg_bob[1], 1)
 circuit.z(qreg_clara[0]).c_if(creg_bob[0], 1)
+
+# circuit.cx(qreg_clara[0], qreg_bob[1])
+# circuit.cz(qreg_clara[0], qreg_bob[0])
+
 circuit.draw()
 plt.savefig("circuit.svg")
 
@@ -53,19 +56,20 @@ circuit.save_statevector()
 result = aer_sim.run(circuit).result()
 counts = result.get_statevector()
 
-# run circuit on actual quantum computer
-IBMQ.load_account()
+### real functionality not supported yet
+# # run circuit on actual quantum computer
+# IBMQ.load_account()
 
-# get the least busy backend
-provider = IBMQ.get_provider(hub='ibm-q')
-backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= 5 and not x.configuration().simulator and x.status().operational))
-print("Running on least busy backend:", backend)
+# # get the least busy backend
+# provider = IBMQ.get_provider(hub='ibm-q')
+# backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= 5 and not x.configuration().simulator and x.status().operational))
+# print("Running on least busy backend:", backend)
 
-# run circuit
-transpiled_circuit = transpile(circuit, backend, optimization_level=3)
-job = backend.run(transpiled_circuit)
-result = job.result()
-counts = result.get_statevector()
+# # run circuit
+# transpiled_circuit = transpile(circuit, backend, optimization_level=3)
+# job = backend.run(transpiled_circuit)
+# result = job.result()
+# counts = result.get_statevector()
 
 plot_bloch_multivector(partial_trace(counts, [0,1,2,3]))
 plt.savefig("final_qubit.svg")
